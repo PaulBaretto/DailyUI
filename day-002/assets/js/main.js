bceeb7f319d9d58c17311b0c59191a9915ecd0b3
Paul Baretto <paulbaretto827@gmail.com>
2024-02-17 04:31:24 +0530

    Working on dynamicly add/Sub cost price functionlity

--- day-002/assets/js/main.js
+++ day-002/assets/js/main.js
@@ -1,27 +1,45 @@
 
 
 let incressButtons = document.querySelectorAll('.incress-btn')
-let decressButtons = document.querySelectorAll('.decress-btn')
+let decreaseButtons = document.querySelectorAll('.decress-btn')
 
+let perProduct = 100;
 
 incressButtons.forEach((plusBtn) => {
     plusBtn.addEventListener('click', (e) => {
+        
         let totalProducts = e.currentTarget.nextElementSibling;
         if(totalProducts && totalProducts.tagName === 'INPUT'){
-        //    let  t = ++1
-            // totalProducts.value =  t;
-            totalProducts.value =  parseInt(totalProducts.value) + 1;
+            let value = parseInt(totalProducts.value);
+            if(value < 5){
+                totalProducts.value =  value + 1 ;
+            }else{
+                e.preventDefault()
+            }
+        }
+        
+        let plusButton = e.target.closest('button');
+        let productHolder = plusButton.parentElement.parentElement;
+        let productCost = productHolder.nextElementSibling;
+        console.log(productCost);
+        
+    })
+})
+
+decreaseButtons.forEach((minusBtn) => {
+    minusBtn.addEventListener('click', (e) => {
+        let totalProducts = e.currentTarget.previousElementSibling;
+        if(totalProducts && totalProducts.tagName === 'INPUT'){
+            let value =  parseInt(totalProducts.value);
+            if(value > 1){
+                totalProducts.value = value - 1;
+            }
         }
         
     })
 })
 
-// decressButtons.forEach((minusBtn) => {
-//     minusBtn.addEventListener('click', (e) => {
-//         let totalProducts = e.currentTarget.nextElementSibling;
-//         if(totalProducts && totalProducts.tagName === 'INPUT'){
-//             totalProducts.value =  parseInt(totalProducts.value) - 1;
-//         }
-        
-//     })
-// })
+
+// function test(){
+//    console.log(e.currentTarget)
+// }
